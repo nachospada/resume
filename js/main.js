47dcb27f3ae1eb3df626155923aa77bd8e6575e4
Nacho Spadaccini <44822250+nachospada@users.noreply.github.com>
2018-12-03 13:39:18 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -50,4 +50,24 @@ function toggleMenu() {
         document.getElementsByClassName('btn-line')[1].style.opacity = "1";
         document.getElementsByClassName('btn-line')[2].style.transform = "none";          
     }
-}
+}   
+
+$(document).ready(function(){
+    $(document).on("scroll", onScroll);
+    
+    function onScroll(event){
+        var scrollPos = $(document).scrollTop();
+        
+        $('#navbar a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));        
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('#navbar ul li a').removeClass("active");
+                currLink.addClass("active");
+            }
+            else{
+                currLink.removeClass("active");
+            }
+        });
+    }
+}); 
